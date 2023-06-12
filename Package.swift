f1dfd91751d1b5dba7222ece33763cfffa98e219
Pallab Maiti <pallab@rudderstack.com>
2023-06-12 16:37:32 +0530

    feat: added SPM support (#9)

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "RudderFirebase",
+    platforms: [
+        .iOS("13.0"), .tvOS("12.0"), .macOS("10.13")
+    ],
+    products: [
+        .library(
+            name: "RudderFirebase",
+            targets: ["RudderFirebase"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", "9.2.0"..<"9.2.1"),
+        .package(name: "Rudder", url: "https://github.com/rudderlabs/rudder-sdk-ios", from: "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "RudderFirebase",
+            dependencies: [
+                .product(name: "FirebaseAnalytics", package: "Firebase"),
+                .product(name: "Rudder", package: "Rudder"),
+            ],
+            path: "Sources",
+            sources: ["Classes/"]
+        )
+    ]
+)
--- Podfile
+++ Podfile
@@ -4,24 +4,19 @@ use_frameworks!
 inhibit_all_warnings!
 platform :ios, '13.0'
 
-def shared_pods
-    pod 'Rudder', '~> 2.2.2'
-end
 
 target 'RudderFirebase' do
     project 'RudderFirebase.xcodeproj'
-    shared_pods
+    pod 'Rudder', '~> 2.0'
     pod 'FirebaseAnalytics', '9.2.0'
 end
 
 target 'SampleAppObjC' do
     project 'Examples/SampleAppObjC/SampleAppObjC.xcodeproj'
-    shared_pods
     pod 'RudderFirebase', :path => '.'
 end
 
 target 'SampleAppSwift' do
     project 'Examples/SampleAppSwift/SampleAppSwift.xcodeproj'
-    shared_pods
     pod 'RudderFirebase', :path => '.'
 end
